536637e74f6ced528ea7bb31c180b28932f3c737
Tyler Aldrich <tyler.aldrich@architect.io>
2022-11-30 13:39:29 -0500

    reformat database connection string

--- server/database.go
+++ server/database.go
@@ -16,7 +16,9 @@ type Movie struct {
 }
 
 func configureDb() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
